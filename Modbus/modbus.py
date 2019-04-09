c53e93a2fb172af920d90a2c7daf1eac3aac87a5
BentouDev <bentkowski.jakub@gmail.com>
2019-04-10 00:06:27 +0200

    Fix modbus

--- Modbus/modbus.py
+++ Modbus/modbus.py
@@ -1,18 +1,10 @@
-from flask import current_app, g, session
-from flask.cli import with_appcontext
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
 def get_modbus(address):
-	if 'modbus' not in g:
-		if not address:
-			raise Exception("Unable to connect to modbus when no address!")
-		else:
-			g.modbus = ModbusClient(address, port=502, timeout=10)
-			if not g.modbus.connect():
-				raise Exception("Unable to connect to modbus, check log for errors")
-	return g.modbus
-
-def disconnect_modbus():
-	modbus = g.pop('modbus', None)
-	if modbus is not None:
-		modbus.close()
+	if not address:
+		raise Exception("Unable to connect to modbus when no address!")
+	else:
+		modbus = ModbusClient(address, port=502, timeout=10)
+		if not modbus.connect():
+			raise Exception("Unable to connect to modbus, check log for errors")
+	modbus
